06d8ae551f5019fb54f6eee64635a106fb94003b
suzukiarisa <ariko0325@gmail.com>
2020-03-15 09:28:17 +0000

    create-post-example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-
-		#deviseで利用出来るパラメーターを設定
-		before_action :configure_permitted_parameters, if: :devise_controller?
+#deviseで利用出来るパラメーターを設定
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 
